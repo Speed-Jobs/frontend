3b4b1c3a861d7a520085ca02a87e18252f4ea05a
hwiyoon20010309 <hwiyoon01@naver.com>
2025-12-19 09:14:48 +0900

    feat: 스킬 막대그래프 색상 변경

--- components/dashboard/CompanySkillDiversityChart.tsx
+++ components/dashboard/CompanySkillDiversityChart.tsx
@@ -96,3 +96,4 @@ export default function CompanySkillDiversityChart({ data, isLoading, error }: C
 
 
 
+
--- components/dashboard/TechStackList.tsx
+++ components/dashboard/TechStackList.tsx
@@ -54,3 +54,4 @@ export default function TechStackList({ items }: TechStackListProps) {
 
 
 
+
